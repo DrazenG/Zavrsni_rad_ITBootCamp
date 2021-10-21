d1113a08373b42459e1a8afe6b17446e6da23951
DrazenG <71284011+DrazenG@users.noreply.github.com>
2021-10-21 23:59:48 +0200

    Updated beforeMethod, reworked afterMethod

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -1,10 +1,20 @@
 package tests;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.collections4.FactoryUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -19,6 +29,7 @@ import pages.ProfilePage;
 public abstract class BasicTest {
 	protected WebDriver driver;
 	protected WebDriverWait wait;
+	protected JavascriptExecutor js;
 	
 	// Pages - Stranice koje se unose i povezuju na package "Pages" klikom na njih u okviru menija
 	
@@ -58,17 +69,41 @@ public abstract class BasicTest {
 		this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		this.driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		wait = new WebDriverWait(driver, 10);
+		Actions actions = new Actions(driver);
 		
-		locationPopupPage = new LocationPopupPage(driver, wait);
-		loginPage = new LoginPage(driver, wait);
-		profilePage = new ProfilePage(driver, wait);
+		locationPopupPage = new LocationPopupPage(driver, wait, js, actions);
+		loginPage = new LoginPage(driver, wait, js, actions);
+		profilePage = new ProfilePage(driver, wait, js, actions);
+		authPage = new AuthPage(driver, wait, js, actions);
+		mealPage = new MealPage(driver, wait, js, actions);
+		notificationSystemPage = new NotificationSystemPage(driver, wait, js, actions);
+		cartSummaryPage = new CartSummaryPage(driver, wait, js, actions);
+		//searchResultPage = new SearchResultPage(driver, wait, js, actions);
+		//Ovde treba() eventualno, ako se stigne uneti zadnju stavku  i skinuti dve kose crte. 
 		
 	}
 	
 	@AfterMethod
-	public void afterMethod() throws InterruptedException {
-		Thread.sleep(2500);
-		//driver.quit();
-		//driver.close();
+	public void afterMethod(ITestResult result) throws InterruptedException {
+
+		// Za kompletno resenje ovoga su mi trebale neke biblioteke za FILEUTILS koje nisam uspeo da pronadjem... :/
+		//Tako da ovo ne radi
+		if (ITestResult.FAILURE == result.getStatus()) {
+			try {
+	
+				TakesScreenshot screenshot = (TakesScreenshot) driver;
+			
+				File src = screenshot.getScreenshotAs(OutputType.FILE);
+				
+				String timestamp = new SimpleDateFormat("yyyy_MM_dd__hh_mm_ss").format(new Date(0));
+				//FileUtils.copyFile(src, new File("screenshots\\" + result.getName() + " - " + timestamp + ".png"));
+				System.out.println("Successfully captured a screenshot");
+			} catch (Exception e) {
+				System.out.println("Exception while taking screenshot " + e.getMessage());
+			}
+		}
+		Thread.sleep(3200);
+		driver.quit();
+
 	}
 }
