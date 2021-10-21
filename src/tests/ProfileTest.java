7978f6a289b961646ebab79ff78bdd4ef2dd3adf
DrazenG <71284011+DrazenG@users.noreply.github.com>
2021-10-22 00:02:11 +0200

    Reworked methods

--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.awt.AWTException;
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -9,48 +12,50 @@ import pages.BasicPage;
 public class ProfileTest extends BasicTest {
 	
 	
-	@Test
+	@Test (priority = 3)
 	
 	public void editProfileTest() throws InterruptedException {		
 		driver.get("http://demo.yo-meals.com/guest-user/login-form");
 		locationPopupPage.clickGetCloseElement();
 		loginPage.login(username, password);
 		String expectedLoginMessage = "Login Successfull";
-		Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedLoginMessage));
+		Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedLoginMessage), "ERROR, no element");
 		
 		driver.get("http://demo.yo-meals.com/member/profile");
 		profilePage.changeAllBasicUserInputs("Drazen", "Gajic", "Ulica Neka 22", 060323444, 22408, "Serbia", "Serbia", "Ruma");
 		String expectedSaveMessage = "Setup Successful";
-		Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedSaveMessage));
+		Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedSaveMessage), "ERROR, no element");
 		authPage.userLogout();
 		String expectedLogoutMessage = "Logout Successfull!";
-		Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedLogoutMessage));
+		Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedLogoutMessage), "ERROR, no element");
 	}
 	
-		@Test
+		@Test (priority = 4)
 		
-		public void changeProfileImage() throws InterruptedException {		
+		public void changeProfileImage() throws InterruptedException, IOException, AWTException {		
 			driver.get("http://demo.yo-meals.com/guest-user/login-form");
 			locationPopupPage.clickGetCloseElement();
 			loginPage.login(username, password);
 			String expectedLoginMessage = "Login Successfull";
-			Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedLoginMessage));
+			Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedLoginMessage), "ERROR, no element");
 			driver.get("http://demo.yo-meals.com/member/profile");
+			notificationSystemPage.waitUntilElementNotPresent();
 			
-			// ovde treba UBACITI SLIKU!!!!!!!!!!
+			
+			profilePage.UploadPhoto();
 			
 			String expectedSavedImageMessage = "Profile Image Uploaded Successfully";
-			Assert.assertTrue(profilePage.getUploadImageText().contains(expectedSavedImageMessage));
+			Assert.assertTrue(profilePage.getUploadImageText().contains(expectedSavedImageMessage), "ERROR, no element");
 			profilePage.waitUntilElementNotPresent();
 			
-			profilePage.deleteProfilePicture();
+			profilePage.removePhoto();
 			String expectedDeletedImageMessage = "Profile Image Deleted Successfully";
-			Assert.assertTrue(profilePage.getDeletedImageText().contains(expectedDeletedImageMessage));
+			Assert.assertTrue(profilePage.getDeletedImageText().contains(expectedDeletedImageMessage), "ERROR, no element");
 			profilePage.waitUntilElementNotPresent();
 			
 			authPage.userLogout();
 			String expectedLogoutMessage = "Logout Successfull!";
-			Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedLogoutMessage));
+			Assert.assertTrue(notificationSystemPage.getNotificationMessageText().contains(expectedLogoutMessage), "ERROR, no element");
 		
 }
 
